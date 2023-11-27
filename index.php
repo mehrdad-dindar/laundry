8d717125229338d041f9e55039ffad61bfdbf9c4
Mehrdad Dindar <mehrdad.replay@gmail.com>
2023-11-27 21:08:04 +0330

    install and test prettier

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <head>
+    <meta charset="UTF-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Document</title>
-	<link rel="stylesheet" href="<?= get_template_directory_uri()."/style.css"?>">
-</head>
-<body class="bg-red-300">
-
-</body>
+    <?php wp_head(); ?>
+  </head>
+  <body class="bg-blue-500 p-3 pt-2"></body>
 </html>
